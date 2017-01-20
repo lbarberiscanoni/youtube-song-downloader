3c65ddc016dfe222d760158fa23764b636bf70e1
lbarberiscanoni <hllbck7@gmail.com>
2017-01-20 00:07:12 -0800

    made it into a class

--- songDownload.py
+++ songDownload.py
@@ -5,19 +5,24 @@ import ast
 
 url = sys.argv[1]
 
-def convert(x):
-    subprocess.call("ffmpeg -i " + x + ".mp4 -c:a copy -vn -sn " + x + ".m4a", shell=True)
-    new = str(x).replace("_", "\ ")
-    subprocess.call("mv " + str(x) + ".m4a " + new + ".m4a", shell=True)
-    subprocess.call("rm " + x + ".mp4", shell=True)
+class Downloader():
 
-def download(a, b):
-    subprocess.call("youtube-dl " + a + " -f mp4 -o " + b + ".mp4", shell=True)
-    print "----------------------"
-    convert(b)
+    def __init__(self, url):
+        self.url = url
 
-def getSongName(a):
-    af = subprocess.Popen("youtube-dl -f mp4 --get-filename -o '%(title)s.%(ext)s' " +  a + " --restrict-filenames", stdout=subprocess.PIPE, shell=True)
-    download(url, af.communicate()[0].replace("\n", "").replace(".mp4", ""))
+    def convert(self, x):
+        subprocess.call("ffmpeg -i " + x + ".mp4 -c:a copy -vn -sn " + x + ".m4a", shell=True)
+        new = str(x).replace("_", "\ ")
+        subprocess.call("mv " + str(x) + ".m4a " + new + ".m4a", shell=True)
+        subprocess.call("rm " + x + ".mp4", shell=True)
+
+    def download(self, name):
+        subprocess.call("youtube-dl " + self.url + " -f mp4 -o " + name + ".mp4", shell=True)
+        print "----------------------"
+        self.convert(name)
+
+    def getSongName(self):
+        af = subprocess.Popen("youtube-dl -f mp4 --get-filename -o '%(title)s.%(ext)s' " +  self.url + " --restrict-filenames", stdout=subprocess.PIPE, shell=True)
+        self.download(af.communicate()[0].replace("\n", "").replace(".mp4", ""))
             
-getSongName(url)
+Downloader(url).getSongName()
